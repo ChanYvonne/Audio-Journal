064dd9569565d607ddc4b79d9db832726bb09dd4
ChanYvonne <ychan1352@gmail.com>
2025-10-24 22:09:22 -0700

    Initial Commit

---
+++ AudioJournal/AudioJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioJournalApp.swift
+//  AudioJournal
+//
+//  Created by Yvonne Chan on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
